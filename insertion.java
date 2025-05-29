379681981eb836e8d8dc3d0455d30b1cb25cf573
Varun Joshi <varunjoshi1127@gmail.com>
2025-05-29 15:08:28 +0530

    Insertion sort

---
+++ insertion.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class insertion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        // Taking input from the user
+        System.out.println("Enter the elements of the array : ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // sorting the array elements
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+
+        System.out.println("The sorted array is : ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        sc.close();
+    }
+}

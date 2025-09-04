a67998efee6086b0fe8217df6b40b47396fcc37f
varunjoshi27 <varunjoshi1127@gmail.com>
2025-09-04 15:09:12 +0530

    Updated the code

--- digit.java
+++ digit.java
@@ -1,11 +1,9 @@
 public class digit {
 
     public static void main(String[] args) {
-        int n = 6;
+        int n = 6325345;
         int count = 0;
         while (n > 0) {
-            int last = n % 10;
-            System.out.println(last);
             n = n / 10;
             count++;
 

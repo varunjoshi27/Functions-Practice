4f6bcfb1f5c3dfca69f2d49f7e40f6180bc0050b
varunjoshi27 <varunjoshi1127@gmail.com>
2026-02-11 00:07:54 +0530

    Create the array folder and initializing the array

---
+++ Array/array.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class array {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
